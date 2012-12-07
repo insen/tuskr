5d5655c172474638aa0b2e37132ada57701ef7e9
indranil.sengupta <indranil.sengupta@gmail.com>
2012-12-07 13:07:36 +0530

    add workflow seems to be all hooked up now. added missing files

---
+++ src/Tuskr.Web/Scripts/View_Scripts/Task/AddTask.js
@@ -0,0 +1,16 @@
+﻿window.tuskr = window.tuskr || {};
+
+(function (addTask, $, undefined) {
+    addTask.init = function (callee, $dialog) {
+        $('#submit', $dialog).click(function () {
+            $('form', $dialog).submit();
+        });
+        
+        $('#close', $dialog).click(function () {
+            $dialog.dialog('close');
+            callee.resume();
+        });
+    };
+}(window.tuskr.addTask = window.tuskr.addTask || {}, jQuery));
+
+
